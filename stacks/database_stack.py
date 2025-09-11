20d510154ac7823c1d91e4187704682a6afd2178
Nel Toms <aitoolkit76@gmail.com>
2025-09-11 13:12:19 -0500

    refactor: update workflow inputs structure and add post-deployment notification

--- stacks/database_stack.py
+++ stacks/database_stack.py
@@ -30,7 +30,7 @@ class DatabaseStack(Stack):
         self.db_instance = rds.DatabaseInstance(
             self, f"StorefrontPostgres-{environment}",
             engine=rds.DatabaseInstanceEngine.postgres(
-                version=rds.PostgresEngineVersion.VER_16_3
+                version=rds.PostgresEngineVersion.VER_15_8
             ),
             vpc=vpc,
             credentials=credentials,
